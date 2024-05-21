19eb4c21f851f528b82084fc4a913ed5ac1f5fa7
zzuse <zzuse@outlook.com>
2024-05-21 00:07:58 -0500

    first commit

---
+++ rts/src/index.tsx
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom/client';
+
+const el = document.getElementById("root");
+const root = ReactDOM.createRoot(el!);
+const App = () => {
+    return (
+        <div>
+            <h1>Hi there!</h1>
+        </div>
+    );
+};
+
+root.render(<App />);
