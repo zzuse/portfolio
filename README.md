093a8bca56c9a682e6e8e5243d244f8f354ecefc
zzuse <zzuse@outlook.com>
2024-05-22 22:49:46 -0500

    user search

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
+```
 npx create-react-app rts --template typescript
+```
+
+# content
+chapter 15. commit 69832440cf89a7d2156d17cfea770f502ad3bc70
--- rts/src/index.tsx
+++ rts/src/index.tsx
@@ -1,12 +1,13 @@
 import ReactDOM from 'react-dom/client';
 import GuestList from './state/GuestList';
+import UserSearch from './state/UserSearch';
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el!);
 const App = () => {
     return (
         <div>
-            <GuestList />
+            <UserSearch />
         </div>
     );
 };
