f564e57818a3137958e86cd7b03f8e8b06aa1d01
zzuse <zzuse@outlook.com>
2024-05-22 23:10:23 -0500

    drag event

--- rts/src/index.tsx
+++ rts/src/index.tsx
@@ -1,13 +1,13 @@
 import ReactDOM from 'react-dom/client';
-import GuestList from './state/GuestList';
-import UserSearch from './state/UserSearch';
+
+import EventComponent from './events/EventComponent';
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el!);
 const App = () => {
     return (
         <div>
-            <UserSearch />
+            <EventComponent />
         </div>
     );
 };
